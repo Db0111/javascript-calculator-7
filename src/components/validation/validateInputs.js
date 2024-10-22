f840d717e04ca133f7be56be8eaa62e626f6800e
deokbin <deokbin@naver.com>
2024-10-22 23:46:10 +0900

    refactor: 음수 처리 로직 수정

--- src/components/validation/validateInputs.js
+++ src/components/validation/validateInputs.js
@@ -1,8 +1,8 @@
 /* eslint-disable operator-linebreak */
 export default function validateInputs(inputs) {
-    //  입력값이 없을 때
+    //  입력값이 없을 때 : 0으로 처리
     if (inputs === '') {
-        throw new Error('[ERROR] 입력된 값이 없습니다.');
+        return true;
     }
     //  그냥 숫자만 입력했을 때
     const numTest = /^[0-9]+$/;
@@ -10,10 +10,6 @@ export default function validateInputs(inputs) {
         return true;
     }
 
-    //  커스텀 구분자가 있는지 확인
-    if (inputs.includes('//') === true && inputs.includes('\\n') === true) {
-        return true;
-    }
     //  음수가 입력되어 있는지 확인
     if (inputs.includes('-')) {
         //  '-'가 구분자로 사용되었을 때는 정상으로 판단
@@ -22,6 +18,16 @@ export default function validateInputs(inputs) {
         }
         //  이외의 경우 음수로 판단
         throw new Error('[ERROR] 음수는 입력할 수 없습니다.');
+    }
+
+    //  커스텀 구분자가 있는지 확인
+    if (inputs.includes('//') && inputs.includes('\\n')) {
+        return true;
+    }
+
+    // 구분자만 있을 때
+    if (inputs.trim() && (inputs === ',' || inputs === ':')) {
+        throw new Error('[ERROR] 구분자만 입력할 수 없습니다.');
     }
 
     //  입력값에 숫자가 아닌 값 확인
@@ -34,13 +40,6 @@ export default function validateInputs(inputs) {
             throw new Error(
                 '[ERROR] 구분자가 잘못되었거나, 숫자가 아닌 값이 포함되어 있습니다.',
             );
-        }
-        // 구분자만 있을 때
-        if (
-            Number.isNaN(Number(input)) === true &&
-            (input === ',' || input === ':')
-        ) {
-            throw new Error('[ERROR] 구분자만 입력할 수 없습니다.');
         }
     });
 
