db844f3fd5797ea17da3374b3e9a7d12e0b79f16
deokbin <deokbin@naver.com>
2024-10-22 23:44:46 +0900

    refactor: 구분자를 상수로 선언

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@
 import { Console } from '@woowacourse/mission-utils';
 import splitInputs from './components/split/splitInputs.js';
 import splitCustom from './components/split/splitCustom.js';
+import Calculator from './components/calculation/calculator.js';
 import validateInputs from './components/validation/validateInputs.js';
 
+const DEFAULT_DELIMETERS = [',', ':'];
 class App {
     async run() {
         const inputs = await Console.readLineAsync(
@@ -15,14 +17,17 @@ class App {
                 inputs.includes('//') === true ||
                 inputs.includes('\\n') === true
             ) {
-                Console.print(`결과 : ${splitCustom(inputs)}`);
+                Console.print(`결과 : ${Calculator(splitCustom(inputs))}`);
             } else if (
-                inputs.includes(':') === true &&
-                inputs.includes(',') === true
+                DEFAULT_DELIMETERS.some((delimeter) =>
+                    inputs.includes(delimeter),
+                )
             ) {
-                Console.print(`결과 : ${splitInputs(inputs)}`);
+                Console.print(`결과 : ${Calculator(splitInputs(inputs))}`);
             } else if (typeof Number(inputs) === 'number') {
                 Console.print(`결과 : ${inputs}`);
+            } else if (inputs === '') {
+                Console.print(`결과 : 0`);
             }
         }
     }
